18a853fbf54bc657184ddfc2fa1ed63d27909af4
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-02-13 12:23:14 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -71,7 +71,7 @@ class CommonTsetlinMachine():
 
 		self.number_of_ta_chunks = int((self.hierarchy_structure[-1] - 1) / 32 + 1)
 
-		print("TA_CHUNKS", self.number_of_ta_chunks)
+		print("TA_CHUNKS, FEATURES", self.number_of_ta_chunks, self.hierarchy_structure[-1])
 
 		self.X_train = np.array([])
 		self.Y_train = np.array([])
