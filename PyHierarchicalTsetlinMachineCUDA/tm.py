9341fe4ba412ea19bd1721ec8223a7ca88a899fe
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-02-22 10:09:01 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -80,16 +80,16 @@ class CommonTsetlinMachine():
 			self.number_of_literal_chunks_per_leaf = int((self.number_of_literals_per_leaf - 1) / 32 + 1)
 			self.number_of_literals_per_leaf = self.hierarchy_structure[0][1]*2
 
-		self.hierarchy_size[0] = self.number_of_ta_chunks_per_leaf * self.hierarchy_size[1]
+		self.hierarchy_size[0] = self.number_of_literal_chunks_per_leaf * self.hierarchy_size[1]
 
-		self.number_of_literal_chunks = self.number_of_ta_chunks_per_leaf
+		self.number_of_literal_chunks = self.number_of_literal_chunks_per_leaf
 		for d in range(self.depth - 1, 1, -1):
 			if (self.hierarchy_structure[d][0] == OR_GROUP or self.hierarchy_structure[d][0] == AND_GROUP):
 				self.number_of_literal_chunks *= self.hierarchy_structure[d][1]
 		
 		print("LITERAL_CHUNKS", self.number_of_literal_chunks)
 		
-		print("TA_CHUNKS, TA_CHUNKS_PER_LEAF, FEATURES", self.hierarchy_size[0], self.number_of_ta_chunks_per_leaf, self.hierarchy_structure[0])
+		print("TA_CHUNKS, LITERAL_CHUNKS_PER_LEAF, FEATURES", self.hierarchy_size[0], self.number_of_literal_chunks_per_leaf, self.hierarchy_structure[0])
 		print(self.hierarchy_size)
 		print(self.hierarchy_structure)
 
@@ -261,7 +261,7 @@ class CommonTsetlinMachine():
 	#define PATCHES %d
 
 	#define NUMBER_OF_EXAMPLES %d
-""" % (self.number_of_outputs, self.number_of_clauses, self.hierarchy_size[0], self.number_of_ta_chunks_per_leaf, self.number_of_features, self.number_of_literal_chunks, self.number_of_state_bits, self.boost_true_positive_feedback, self.s, self.T, self.q, self.negative_clauses, self.number_of_patches, number_of_examples)
+""" % (self.number_of_outputs, self.number_of_clauses, self.hierarchy_size[0], self.number_of_literal_chunks_per_leaf, self.number_of_features, self.number_of_literal_chunks, self.number_of_state_bits, self.boost_true_positive_feedback, self.s, self.T, self.q, self.negative_clauses, self.number_of_patches, number_of_examples)
 
 			mod_prepare = SourceModule(parameters + kernels.code_header + kernels.code_prepare, no_extern_c=True)
 			self.prepare = mod_prepare.get_function("prepare")
