55b7461e0843dd64c98e116f8f02495a34490e23
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-02-23 13:38:54 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -567,7 +567,7 @@ code_encode = """
 			unsigned int *Xi;
 			unsigned int *encoded_Xi;
 
-/*			for (unsigned long long i = index; i < number_of_examples; i += stride) {
+			for (unsigned long long i = index; i < number_of_examples; i += stride) {
 				Xi = &X[i*number_of_literals];
 				for (int j = 0; j < number_of_leaves; ++j) {
 					for (int k = 0; k < number_of_literals_per_leaf; ++k) {
@@ -579,7 +579,7 @@ code_encode = """
 						}
 					}
 				}
-			}*/
+			}
 		}
 	}
 """
