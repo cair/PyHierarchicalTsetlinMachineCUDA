34f01a212cfe1865d90c369afefc9ab79111f88d
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-02-27 10:28:37 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -318,18 +318,18 @@ code_update = """
 			}
 		}
 
-		__global__ void evaluate_and_groups_final(int *child_input, int number_of_and_group_nodes, int number_of_and_factors, int *clause_weights, int *class_sum)
+		__global__ void evaluate_and_groups_final(int *child_input, int number_of_and_group_factors, int *clause_weights, int *class_sum)
 		{
 			int index = blockIdx.x * blockDim.x + threadIdx.x;
 			int stride = blockDim.x * gridDim.x;
 
 			// Add up the votes of each OR node
-			for (int and_group_node = index; and_group_node < CLAUSES*number_of_and_group_nodes; and_group_node += stride) {
+			for (int clause = index; clause < CLAUSES; clause += stride) {
 				// Multiply and factors
 				int and_group_vote_product = 1;
-				for (int and_factor = 0; and_factor < number_of_and_factors; ++and_factor) {
+				for (int and_factor = 0; and_factor < number_of_and_group_factors; ++and_factor) {
 					// Aggregate votes from each child node through multiplication
-					and_group_vote_product *= child_input[and_group_node*number_of_and_factors + and_factor];
+					and_group_vote_product *= child_input[and_group_node*number_of_and_group_factors + and_factor];
 				}
 
 				if (and_group_vote_product) {
