fdac536a55db9c4996fd7d065c289921ea4043f3
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-05 10:41:41 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -59,7 +59,7 @@ class CommonTsetlinMachine():
 		self.hierarchy_size[self.depth] = 1
 		print(self.depth)
 		for d in range(self.depth - 1):
-			self.hierarchy_size[self.depth - d - 1] = self.hierarchy_structure[self.depth - d - 1][1] * self.hierarchy_size[self.depth - d - 1]
+			self.hierarchy_size[self.depth - d - 1] = self.hierarchy_structure[self.depth - d - 1][1] * self.hierarchy_size[self.depth - d]
 			print(self.depth - d - 1, self.hierarchy_size[self.depth - d - 1])
 		print(self.hierarchy_size)
 
