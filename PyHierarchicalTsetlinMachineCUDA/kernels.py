3e7b1707aaa317e46b23f86a070e65affa07afc0
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-11 19:28:47 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -258,7 +258,7 @@ code_update = """
 
 			ta_chunks_size[0] = hierarchy_structure_factors[0];
 			if (index == 0) {
-				printf("*%d: %d\\n", d, ta_chunks_size[0]);
+				printf("*%d: %d\\n", 0, ta_chunks_size[0]);
 			}
 			for (int d = 1; d < depth-1; ++d) {
 				ta_chunks_size[d] = ta_chunks_size[d-1] * hierarchy_structure_factors[d-1];
