3fa50b08f94fff96636cf9bb9f2642792aea1711
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-25 09:31:01 +0100

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -903,8 +903,8 @@ code_prepare = """
 			for (unsigned long long clause = index; clause < CLAUSES; clause += stride) {
 				for (unsigned long long class_id = 0; class_id < CLASSES; ++class_id) {
 					#if NEGATIVE_CLAUSES == 1
-						clause_weights[class_id*CLAUSES + clause] = 1 - 2 * (curand(&localState) % 2);
-						//clause_weights[class_id*CLAUSES + clause] = 1 - 2 * (clause % 2);
+						//clause_weights[class_id*CLAUSES + clause] = 1 - 2 * (curand(&localState) % 2);
+						clause_weights[class_id*CLAUSES + clause] = 1 - 2 * (clause % 2);
 					#else
 						clause_weights[class_id*CLAUSES + clause] = 1;
 					#endif
