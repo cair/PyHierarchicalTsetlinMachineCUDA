8cd60267652ef4766bd23b7e8896eb4fa160aad9
Mayur1009 <mayurks@uia.no>
2026-03-26 09:18:57 +0100

    Adding dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from setuptools import *
+from setuptools import setup
 
 setup(
-   name='PyHierarchicalTsetlinMachineCUDA',
-   version='0.2.0',
-   author='Ole-Christoffer Granmo',
-   author_email='ole.granmo@uia.no',
-   url='https://github.com/cair/PyHierarchicalTsetlinMachineCUDA/',
-   license='MIT',
-   description='Hierarchical Tsetlin Machine Architecture.',
-   long_description='Hierarchical Tsetlin Machine Architecture.',
-   keywords ='pattern-recognition cuda machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic tsetlin-machine regression convolution classification multi-layer',
-   packages=['PyHierarchicalTsetlinMachineCUDA']
+	name='PyHierarchicalTsetlinMachineCUDA',
+	version='0.2.0',
+	author='Ole-Christoffer Granmo',
+	author_email='ole.granmo@uia.no',
+	url='https://github.com/cair/PyHierarchicalTsetlinMachineCUDA/',
+	license='MIT',
+	description='Hierarchical Tsetlin Machine Architecture.',
+	long_description='Hierarchical Tsetlin Machine Architecture.',
+	keywords='pattern-recognition cuda machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic tsetlin-machine regression convolution classification multi-layer',
+	packages=['PyHierarchicalTsetlinMachineCUDA'],
+	install_requires=[
+		'numpy',
+		'pycuda',
+		'scipy',
+		'scikit-learn',
+	],
+	extras_require={
+		'examples': ['tensorflow'],
+	},
 )
