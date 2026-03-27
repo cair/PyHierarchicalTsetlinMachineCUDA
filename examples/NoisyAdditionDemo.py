439790d45a4c651dd1974bd75370487b5dae9d02
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-27 13:53:25 +0100

    Update

--- examples/NoisyAdditionDemo.py
+++ examples/NoisyAdditionDemo.py
@@ -4,7 +4,7 @@ from time import time
 import PyHierarchicalTsetlinMachineCUDA.tm as tm
 
 noise = 0.0
-number_of_addends = 4
+number_of_addends = 8
 number_of_examples = 10000
 
 X_train = np.random.randint(2, size=(number_of_examples, number_of_addends)).astype(np.int32)
@@ -16,7 +16,7 @@ Y_test = X_test.sum(axis=1)
 
 #tm = MultiClassTsetlinMachine(100, 15*64*10, 20.0, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, number_of_values), (tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 8), (tm.AND_GROUP, 2)))
 #tm = MultiClassTsetlinMachine(10, 15, 4.1, number_of_state_bits=8, boost_true_positive_feedback=1, hierarchy_structure=((tm.AND_GROUP, 2), (tm.AND_GROUP, 2)))
-tm = MultiClassTsetlinMachine(10, 15, 4.1, number_of_state_bits=8, boost_true_positive_feedback=1, hierarchy_structure=((tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 8), (tm.AND_GROUP, 2)))
+tm = MultiClassTsetlinMachine(10, 15, 4.1, number_of_state_bits=8, boost_true_positive_feedback=1, hierarchy_structure=((tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 8), (tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 8), (tm.AND_GROUP, 2)))
 
 print("\nAccuracy over 500 epochs:\n")
 for i in range(500):
