da3fde6d9265c73c8113fdb9b7996ad3f0a7b5e2
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-27 13:31:28 +0100

    Update

--- examples/NoisyAdditionDemo.py
+++ examples/NoisyAdditionDemo.py
@@ -24,7 +24,7 @@ for i in range(number_of_examples):
 	for j in range(number_of_addends):
 		X_test[i, j*number_of_values + X_test_integer[i, j]] = 1
 
-tm = MultiClassTsetlinMachine(10, 15*64, 5.0, number_of_state_bits=8, boost_true_positive_feedback=1, hierarchy_structure=((tm.AND_GROUP, number_of_values), (tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 8), (tm.AND_GROUP, 2)))
+tm = MultiClassTsetlinMachine(10, 15*64, 10.0, number_of_state_bits=8, boost_true_positive_feedback=1, hierarchy_structure=((tm.AND_GROUP, number_of_values), (tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 8), (tm.AND_GROUP, 2)))
 
 print("\nAccuracy over 500 epochs:\n")
 for i in range(500):
