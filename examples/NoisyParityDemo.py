7e447b56bfcf340ddaaa4da7472a2f8b39d81a7c
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-28 17:18:15 +0100

    Update

--- examples/NoisyParityDemo.py
+++ examples/NoisyParityDemo.py
@@ -3,6 +3,8 @@ import numpy as np
 from time import time
 import PyHierarchicalTsetlinMachineCUDA.tm as tm
 
+clauses = 32
+
 train_data = np.loadtxt("./examples/NoisyParityTrainingData.txt").astype(np.uint32)
 X_train = train_data[:,0:-1]
 Y_train = train_data[:,-1]
@@ -11,7 +13,7 @@ test_data = np.loadtxt("./examples/NoisyParityTestingData.txt").astype(np.uint32
 X_test = test_data[:,0:-1]
 Y_test = test_data[:,-1]
 
-tm = TsetlinMachine(32, 3000, 30.1, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 3), (tm.OR_ALTERNATIVES, 10), (tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 2), (tm.AND_GROUP, 2)))
+tm = TsetlinMachine(clauses, 3000, 30.1, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 3), (tm.OR_ALTERNATIVES, 10), (tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 2), (tm.AND_GROUP, 2)))
 
 print("\nAccuracy over 500 epochs:\n")
 for i in range(500):
@@ -23,13 +25,15 @@ for i in range(500):
 	result = 100*(tm.predict(X_test) == Y_test).mean()
 	stop_testing = time()
 
-	for i in range(tm.hierarchy_size[1]):
-		print("Component %d:", end= '')
-		for j in range(tm.number_of_literals_per_leaf):
-			if j < tm.number_of_literals_per_leaf // 2:
-				print(" x%d=%d)", j, tm.ta_action(i, j), end='')
-			else:
-				print(" ¬x%d=%d)", j, tm.ta_action(i, j), end='')
-		print()
+	for i in range(clauses):
+		print("CLAUSE %d", i)
+		for j in range(tm.hierarchy_size[1]):
+			print("\tComponent %d:", end= '')
+			for k in range(tm.number_of_literals_per_leaf):
+				if k < tm.number_of_literals_per_leaf // 2:
+					print(" x%d=%d)", j, tm.ta_action(i, j, k), end='')
+				else:
+					print(" ¬x%d=%d)", j, tm.ta_action(i, j, k), end='')
+			print()
 
 	print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
