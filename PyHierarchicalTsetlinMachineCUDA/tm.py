a0464665100a8f7e23b22b787c6fde86f4cf7486
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-03-31 11:49:01 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -404,7 +404,7 @@ class CommonTsetlinMachine():
 
 	def print_hierarchy(self, print_ta_state=False):
 		for i in range(self.number_of_clauses):
-			print("\n CLAUSE #%d: " % (i), end='')
+			print("\nCLAUSE #%d: " % (i), end='')
 
 			previous_index = np.ones((self.depth-1), dtype=np.int32)*-1
 			for j in range(self.hierarchy_size[1]):
