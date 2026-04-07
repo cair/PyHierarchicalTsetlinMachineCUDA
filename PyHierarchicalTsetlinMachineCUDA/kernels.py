976dead2a6c0851eb9504fcca469b94e0de02eff
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-07 22:59:57 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -304,7 +304,7 @@ code_update = """
 				if (child_input[clause]) {
 					for (int class_id = 0; class_id < number_of_outputs; ++class_id) {
 						int clause_weight = clause_weights[class_id*CLAUSES + clause];
-						atomicAdd(&class_sum[class_id], clause_weight * child_input[clause]);					
+						atomicAdd(&class_sum[class_id], (long long int)clause_weight * child_input[clause]);					
 					}
 				}
 			}
