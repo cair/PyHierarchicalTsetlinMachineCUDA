9ef8541be40f535289418004fe260393ab59ac20
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-07 14:51:10 +0200

    Update

--- examples/HexDemo.py
+++ examples/HexDemo.py
@@ -14,17 +14,17 @@ Y_train = data[:int(len(data)*0.8),-1]
 X_test = data[int(len(data)*0.8):,0:-1]
 Y_test = data[int(len(data)*0.8):,-1]
 
-tm = TsetlinMachine(clauses, T, s, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 25), (tm.OR_ALTERNATIVES, 1), (tm.AND_GROUP, 4)))
-tm = TsetlinMachine(clauses, T, s, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 100), (tm.AND_GROUP, 1)))
+#tsetlin_machine = TsetlinMachine(clauses, T, s, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 25), (tm.OR_ALTERNATIVES, 1), (tm.AND_GROUP, 4)))
+tsetlin_machine = TsetlinMachine(clauses, T, s, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 100), (tm.AND_GROUP, 1)))
 
 print("\nAccuracy over 1000 epochs:\n")
 for e in range(1000):
 	start_training = time()
-	tm.fit(X_train, Y_train, epochs=10, incremental=True)
+	tsetlin_machine.fit(X_train, Y_train, epochs=10, incremental=True)
 	stop_training = time()
 
 	start_testing = time()
-	result = 100*(tm.predict(X_test) == Y_test).mean()
+	result = 100*(tsetlin_machine.predict(X_test) == Y_test).mean()
 	stop_testing = time()
 
 	#tm.print_hierarchy()
