8367a5c5efcb4ecf3c27d27002e178ddef8aa930
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-07 14:55:30 +0200

    Update

--- examples/hex.c
+++ examples/hex.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #ifndef BOARD_DIM
-    #define BOARD_DIM 10
+    #define BOARD_DIM 4
 #endif
 
 int neighbors[] = {-(BOARD_DIM+2) + 1, -(BOARD_DIM+2), -1, 1, (BOARD_DIM+2), (BOARD_DIM+2) - 1};
