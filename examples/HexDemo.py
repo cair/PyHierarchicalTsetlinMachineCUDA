ba4a3b6e17583258df1bdff96e6ef541e17d9f21
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-08 13:22:24 +0200

    Update

--- examples/HexDemo.py
+++ examples/HexDemo.py
@@ -43,7 +43,7 @@ print("\nAccuracy over 1000 epochs:\n")
 for e in range(args.epochs):
 	for b in range(10):
 		start_training = time()
-		tsetlin_machine.fit(X_train[b*len(Y_train)/10:(b+1)*len(Y_train)/10], Y_train[b*len(Y_train)/10:(b+1)*len(Y_train)/10], epochs=1, incremental=True)
+		tsetlin_machine.fit(X_train[b*len(Y_train)//10:(b+1)*len(Y_train)//10], Y_train[b*len(Y_train)//10:(b+1)*len(Y_train)//10], epochs=1, incremental=True)
 		stop_training = time()
 
 		start_testing = time()
