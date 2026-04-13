8dd913b72b65b5151ebc2270b8f54cf41054bdc7
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-13 10:20:53 +0200

    Update

--- examples/MNISTConvolutionDemo.py
+++ examples/MNISTConvolutionDemo.py
@@ -28,7 +28,7 @@ for i in range(X_test.shape[0]):
 	X_test[i,:,:,:] = view_as_windows(X_org_test[i,:,:], (patch_size, patch_size)).reshape((number_of_patches, patch_size, patch_size))
 X_test = X_test.reshape((X_org_test.shape[0], -1))
 
-tm = MultiClassTsetlinMachine(clauses, T, s, hierarchy_structure=((tm.AND_GROUP, patch_size**2), (tm.OR_GROUP, number_of_patches)))
+tm = MultiClassTsetlinMachine(clauses, T, s, hierarchy_structure=((tm.AND_GROUP, 2*patch_size**2), (tm.OR_GROUP, number_of_patches)))
 
 print("\nAccuracy over 500 epochs:\n")
 for i in range(500):
