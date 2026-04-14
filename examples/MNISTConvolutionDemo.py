676c2047d6e8fc7cab9d578ec28b60d248639285
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-14 09:31:14 +0200

    Update

--- examples/MNISTConvolutionDemo.py
+++ examples/MNISTConvolutionDemo.py
@@ -21,7 +21,7 @@ number_of_patches = int((X_org_train.shape[1] - patch_size + 1) * (X_org_train.s
 
 X_train = np.zeros((X_org_train.shape[0], number_of_patches, patch_size * patch_size + (patch_size-1)*2))
 for i in range(X_train.shape[0]):
-	X_train[i,:,:] = view_as_windows(X_org_train[i,:,:patch_size*patch_size], (patch_size, patch_size)).reshape((number_of_patches, patch_size, patch_size))
+	X_train[i,:,:] = view_as_windows(X_org_train[i,:,:patch_size*patch_size], (patch_size, patch_size)).reshape((number_of_patches, patch_size*patch_size))
 X_train = X_train.reshape((X_org_train.shape[0], -1))
 
 X_test = np.zeros((X_org_test.shape[0], number_of_patches, patch_size * patch_size + (patch_size-1)*2))
