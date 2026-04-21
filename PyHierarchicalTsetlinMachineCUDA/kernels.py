ad4ae77caf0a68937c852ef1111b1928f5da8630
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-21 17:18:54 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -388,12 +388,10 @@ code_update = """
 
 			// Add up the votes from each clause
 			for (int clause = index; clause < CLAUSES; clause += stride) {
-				if (child_input[clause] > 0) {
+				if (child_input[clause] != -1) {
 					for (int class_id = 0; class_id < number_of_outputs; ++class_id) {
 						int clause_weight = clause_weights[class_id*CLAUSES + clause];
-						if (child_input[clause] != -1) {
-							atomicAdd(&class_sum[class_id], 1.0*clause_weight * child_input[clause]);
-						}
+						atomicAdd(&class_sum[class_id], 1.0*clause_weight * exp2(child_input[clause]));
 					}
 				}
 			}
