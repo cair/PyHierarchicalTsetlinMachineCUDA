8dd917ecd7b71f4aa20e7af3f76961cab867c5b0
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-22 14:04:09 +0200

    Update

--- examples/MNISTDemoCoalesced.py
+++ examples/MNISTDemoCoalesced.py
@@ -13,7 +13,7 @@ s = 10.0
 X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0)
 X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0)
 
-tm = MultiClassCoalescedTsetlinMachine(clauses, T, s, hierarchy_structure=((tm.AND_GROUP, 28*28), (tm.AND_GROUP, 1)))
+tm = MultiClassCoalescedTsetlinMachine(clauses, T, s, log_scale=True, hierarchy_structure=((tm.AND_GROUP, 28*28), (tm.AND_GROUP, 1)))
 
 print("\nAccuracy over 500 epochs:\n")
 for i in range(500):
