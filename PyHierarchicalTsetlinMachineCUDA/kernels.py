2bb93425582a83a1ff3e7975bad061e06610fd3e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-23 09:20:49 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -689,7 +689,48 @@ code_update = """
 			}
 		}
 
-		__global__ void evaluate_final(int number_of_outputs, float *child_input, int *clause_weights, int *class_sum)
+		__global__ void evaluate_final_log(int number_of_outputs, float *clause_output, int *clause_weights, int *class_sum)
+		{
+			int index = blockIdx.x * blockDim.x + threadIdx.x;
+			int stride = blockDim.x * gridDim.x;
+
+			// Add up the votes from each clause
+			for (int class_id = index; class_id < number_of_outputs; class_id += stride) {
+				#if LOG_SCALE == 1
+					float clause_output_max = NEG_INFINITY;
+					for (int clause = 0; clause < CLAUSES; ++clause) {
+						if (clause_output[clause] > clause_output_max) {
+							clause_output_max = clause_output[clause];
+						}
+					}
+
+					if (clause_output_max != NEG_INFINITY) {
+						float weighted_clause_output_sum = 0;
+						for (int clause = 0; clause < CLAUSES; ++clause) {
+							weighted_clause_output_sum += clause_weights[class_id*CLAUSES + clause] * exp2f(clause_output[clause] - clause_output_max);
+						}
+
+						if (log2f(fabs(weighted_clause_output_sum)) + clause_output_max >= log2f(THRESHOLD)) {
+							float sign = (1 - 2 * (weighted_clause_output_sum < 0));
+							class_sum[class_id] = sign*THRESHOLD;
+						} else {
+							class_sum[class_id] = weighted_clause_output_sum * exp2f(clause_output_max);
+						}
+					} else {
+						class_sum[class_id] = 0;
+					}
+				#else
+					for (int clause = 0; clause < CLAUSES; ++clause) {
+						if (clause_output[clause]) {
+							float clause_weight = clause_weights[class_id*CLAUSES + clause];
+							class_sum[class_id] += clause_weight * clause_output[clause];				
+						}
+					}
+				#endif
+			}
+		}
+
+		__global__ void evaluate_final_old(int number_of_outputs, float *child_input, int *clause_weights, int *class_sum)
 		{
 			int index = blockIdx.x * blockDim.x + threadIdx.x;
 			int stride = blockDim.x * gridDim.x;
