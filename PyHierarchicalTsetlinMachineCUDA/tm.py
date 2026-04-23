04bbf7e0181eaf6fff7ef6d4b1060573d10d93ef
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-04-23 20:09:16 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/tm.py
+++ PyHierarchicalTsetlinMachineCUDA/tm.py
@@ -318,14 +318,15 @@ class CommonTsetlinMachine():
 				printf("Unknown node type!")
 				sys.exit()
 
-		clause_output_max = gpuarray.max(self.clause_output)
+		clause_output_max = gpuarray.max(self.clause_output).get()
+
 		# Adds up the votes from each clause (hierarchy root)
 		self.evaluate_final.prepared_call(
 			self.grid,
 			self.block,
 			np.int32(self.number_of_outputs),
 			self.hierarchy_votes[self.depth-1],
-			np.float32(clause_output_max),
+			clause_output_max,
 			self.clause_weights_gpu,
 			self.class_sum_gpu
 		)
