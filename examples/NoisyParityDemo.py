677d532f44ce6c2e7d29dd547467fe45151c3caa
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-11 13:44:17 +0200

    Update

--- examples/NoisyParityDemo.py
+++ examples/NoisyParityDemo.py
@@ -3,9 +3,9 @@ import numpy as np
 from time import time
 import PyHierarchicalTsetlinMachineCUDA.tm as tm
 
-clauses = 32
-s = 25.0
-T = 250
+clauses = 4
+s = 6.0
+T = 6
 
 train_data = np.loadtxt("./examples/NoisyParityTrainingData.txt").astype(np.uint32)
 X_train = train_data[:,0:-1]
@@ -15,10 +15,11 @@ test_data = np.loadtxt("./examples/NoisyParityTestingData.txt").astype(np.uint32
 X_test = test_data[:,0:-1]
 Y_test = test_data[:,-1]
 
-tm = TsetlinMachine(clauses, T, s, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 4), (tm.AND_GROUP, 2)))
 
 print("\nAccuracy over 1000 epochs:\n")
 for e in range(1000):
+	tm = TsetlinMachine(clauses, T, s, number_of_state_bits=8, boost_true_positive_feedback=0, hierarchy_structure=((tm.AND_GROUP, 2), (tm.OR_ALTERNATIVES, 4), (tm.AND_GROUP, 2)))
+
 	start_training = time()
 	tm.fit(X_train, Y_train)
 	stop_training = time()
@@ -27,6 +28,6 @@ for e in range(1000):
 	result = 100*(tm.predict(X_test) == Y_test).mean()
 	stop_testing = time()
 
-	tm.print_hierarchy()
+	tm.print_hierarchy(print_ta_state=True)
 
 	print("\n#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (e+1, result, stop_training-start_training, stop_testing-start_testing))
