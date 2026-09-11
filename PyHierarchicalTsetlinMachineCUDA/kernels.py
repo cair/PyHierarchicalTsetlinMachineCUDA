5f39073035104a99cd91e0796f8bfb7eb21977f1
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-11 08:46:15 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -435,8 +435,12 @@ code_update = """
 						or_alternatives_vote_sum += child_input[or_alternatives_node * number_of_or_alternatives + or_alternative];
 					}
 
+					if (or_alternatives_vote_sum > T) {
+						or_alternatives_vote_sum = T;
+					}
+
 					// Store vote sum as node output
-					or_alternatives_node_output[or_alternatives_node] = or_alternatives_vote_sum;
+					or_alternatives_node_output[or_alternatives_node] = 1.0*or_alternatives_vote_sum/T;
 				#endif
 			}
 		}
