63eb6c666f9315fc6dd910265ce05f7e2cbb60d3
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-15 12:11:59 +0200

    Update

--- PyHierarchicalTsetlinMachineCUDA/kernels.py
+++ PyHierarchicalTsetlinMachineCUDA/kernels.py
@@ -274,7 +274,7 @@ code_update = """
 					#if LOG_SCALE == 1
 				 		and_group_vote_product += child_input[and_group_node*number_of_and_group_factors + and_factor];
 				 	#else
-				 		and_group_vote_product *= child_input[and_group_node*number_of_and_group_factors + and_factor];
+				 		and_group_vote_product *= powf(child_input[and_group_node*number_of_and_group_factors + and_factor], 1.0/number_of_and_group_factors);
 				 	#endif	
 				}
 				
