8acf6164353c7fbd55b9e26f37d1a5a3060c7205
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-09-16 14:09:03 +0200

    Update

--- examples/ANDOfXORsDemo.py
+++ examples/ANDOfXORsDemo.py
@@ -116,6 +116,7 @@ for i in range(10):
 			args.s,
 			number_of_state_bits=args.number_of_state_bits,
 			boost_true_positive_feedback=0,
+			local_update_p=args.local_update_p,
 			hierarchy_structure=(
 				(tm.AND_GROUP, (2 + args.number_of_irrelevant_features) * args.number_of_ands),
 				(tm.OR_ALTERNATIVES, args.number_of_alternatives)
